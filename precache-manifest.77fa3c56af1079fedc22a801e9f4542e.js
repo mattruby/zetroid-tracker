b5fd65c55b8d8f9c113b0a829d33cc2ed8a0b191
aboyer <aboyer1013@gmail.com>
2018-11-25 21:47:47 -0600

    Updates

---
+++ precache-manifest.77fa3c56af1079fedc22a801e9f4542e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a68896f9551515e09b8e",
+    "url": "/zetroid-tracker/static/js/runtime~main.a68896f9.js"
+  },
+  {
+    "revision": "cea54c18cf3f71cb2eb7",
+    "url": "/zetroid-tracker/static/js/main.cea54c18.chunk.js"
+  },
+  {
+    "revision": "6a22f9ef389d7780b104",
+    "url": "/zetroid-tracker/static/js/1.6a22f9ef.chunk.js"
+  },
+  {
+    "revision": "cea54c18cf3f71cb2eb7",
+    "url": "/zetroid-tracker/static/css/main.117dc2c3.chunk.css"
+  },
+  {
+    "revision": "5cfc26ff93408d11245c2f590bcfea00",
+    "url": "/zetroid-tracker/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/zetroid-tracker/precache-manifest.77fa3c56af1079fedc22a801e9f4542e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/zetroid-tracker/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
