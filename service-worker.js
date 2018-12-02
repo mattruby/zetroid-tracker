28e07e9b18abbb010b844c96ff475ca52fb472e7
aboyer <aboyer1013@gmail.com>
2018-12-02 11:19:39 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/zetroid-tracker/precache-manifest.2e2541821da13954052f9b1c4a974b2e.js"
+  "/zetroid-tracker/precache-manifest.2500e2f73d84e49c93561d36b551693e.js"
 );
 
 workbox.clientsClaim();
